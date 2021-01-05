bb0db6fa482797de14a43b5edf1880f1b0de5cf8
David Peterson <dapete2@outlook.com>
2021-01-05 16:45:26 -0600

    initial commit

---
+++ CreateProduct.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace inhifistereo.httptrigger1
+{
+    public static class CreateProduct
+    {
+        [FunctionName("CreateProduct")]
+        public static void Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "dpfuncbecuwork",
+                collectionName: "productcontainer",
+                ConnectionStringSetting = "CosmosDBConnectionString")]out dynamic document,
+            ILogger log)
+        {
+             
+            document = new { productDescription = "This starfruit ice cream is out of this world!", productId = Guid.NewGuid(), productName = "Starfruit Explosion", timestamp = DateTime.Now };
+
+            log.LogInformation($"C# Queue trigger function inserted one row");
+        }
+    }
+}
